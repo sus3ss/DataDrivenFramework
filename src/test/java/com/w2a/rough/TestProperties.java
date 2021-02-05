8e145b800367b2b5f61719a8755b424f950eb346
sus3ss <Skadaiahnamath@BLR-LAP-015.global.internal>
2021-02-05 18:03:55 +0530

    removed

--- src/test/java/com/w2a/rough/TestProperties.java
+++ src/test/java/com/w2a/rough/TestProperties.java
@@ -24,7 +24,6 @@ public class TestProperties {
 		
 		//driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
 		System.out.println(OR.getProperty("bmlBtn"));
-		System.out.println("hello");
 	}
 
 }
