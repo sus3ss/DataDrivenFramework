9f56a6ef63748c3334b9a9e8021c93e52640f507
sus3ss <Skadaiahnamath@global.internal>
2021-02-05 15:57:21 +0530

    first commit

---
+++ src/test/java/com/w2a/testcases/BankManagerLoginTest.java
@@ -0,0 +1,37 @@
+package com.w2a.testcases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.w2a.base.TestBase;
+
+public class BankManagerLoginTest extends TestBase {
+
+	
+	@Test
+	public void bankManagerLoginTest() throws InterruptedException, IOException{
+		
+		
+		verifyEquals("xyz", "xyz");
+		Thread.sleep(3000);
+		log.debug("Inside Login Test");
+		log.debug("Testing");
+		click("bmlBtn_CSS");
+
+		Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addCustBtn_CSS"))),"Login not successful");
+		
+		log.debug("Login successfully executed");
+		
+		//Assert.fail("Login not successful");
+		
+		
+		
+	
+	
+	}
+	
+}
