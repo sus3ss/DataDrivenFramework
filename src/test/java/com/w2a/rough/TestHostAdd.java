d87e47be11a914b52729e7ca4dc9f6cf96186bec
sus3ss <Skadaiahnamath@BLR-LAP-015.global.internal>
2021-05-21 12:53:32 +0530

    PM Demo

--- src/test/java/com/w2a/rough/TestHostAdd.java
+++ src/test/java/com/w2a/rough/TestHostAdd.java
@@ -17,7 +17,7 @@ public class TestHostAdd {
 		String messageBody = "http://" + InetAddress.getLocalHost().getHostAddress()
 				+ ":8080/job/DataDrivenLiveProject/Extent_Reports/";
 		System.out.println(messageBody);
-		
+		///PM Demo
 		mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
 
 	}
